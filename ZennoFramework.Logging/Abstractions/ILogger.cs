0ddfb69cdafc072b4d94f1aac39a8fbae2c16c53
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-03-15 17:36:12 +0300

    ...

--- ZennoFramework.Logging/Abstractions/ILogger.cs
+++ ZennoFramework.Logging/Abstractions/ILogger.cs
@@ -4,5 +4,7 @@ namespace ZennoFramework.Logging.Abstractions
 {
     public interface ILogger
     {
+        bool IsEnabled(LogLevel logLevel);
+        void Log<TState>(LogLevel logLevel, TState state, Exception exception, Func<TState, Exception, string> formatter);
     }
 }
