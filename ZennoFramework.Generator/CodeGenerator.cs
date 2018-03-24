be236b77d26af42c615ca036521b66db333f53ef
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-03-24 20:14:36 +0300

    ...

---
+++ ZennoFramework.Generator/CodeGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+using ZennoFramework.Api.Common.Messages;
+using ZennoFramework.Generator.Extensions;
+using ZennoFramework.Generator.Internal;
+
+namespace ZennoFramework.Generator
+{
+    public static class CodeGenerator
+    {
+    }
+}
---
+++ ZennoFramework.Generator/Element.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ZennoFramework.Generator
+{
+    internal class Element
+    {
+    }
+}
