8848e2fde2b42fcec7292c601531c07848fa1673
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-04-02 18:37:52 +0300

    ...

--- ZennoFramework.Generator/Internal/Comment.cs
+++ ZennoFramework.Generator/Internal/Comment.cs
@@ -4,5 +4,16 @@ namespace ZennoFramework.Generator.Internal
 {
     public static class Comment
     {
+        internal static string GetComment(string comment)
+        {
+            comment = comment.Replace(Environment.NewLine, Environment.NewLine + "///");
+
+            var result = string.Empty;
+            result += $"/// <summary>{Environment.NewLine}";
+            result += $"/// {comment} {Environment.NewLine}";
+            result += $"/// </summary>";
+
+            return result;
+        }
     }
 }
