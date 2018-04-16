1757d9b18ce45456264225d7af59a8cb42fdfcab
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-04-16 17:11:18 +0300

    Xml парсер

---
+++ ZennoFramework.Xml/XmlParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ZennoFramework.Xml.Extensions;
+using ZennoFramework.Xml.Validation;
+
+namespace ZennoFramework.Xml
+{
+    public class XmlParser
+    {
+    }
+}
