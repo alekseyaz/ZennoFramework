0f7259bf5b16ee772160da773f4a3307ffd35dcf
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-02 22:45:53 +0300

    Клиентский Api

---
+++ ZennoFramework.ApiClient/ApiClient.cs
@@ -0,0 +1,12 @@
+﻿using ZennoFramework.Api.Client.Utils;
+using ZennoFramework.ApiClient.Exceptions;
+using ZennoFramework.Api.Common.Utils;
+using ZennoFramework.Api.Common.Messages;
+
+namespace ZennoFramework.Api.Client
+{
+    public class ApiClient
+    {   
+
+    }   
+}
