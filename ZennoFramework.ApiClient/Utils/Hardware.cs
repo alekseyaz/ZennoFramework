460131ec07765e524963a1648b4330215aa82d94
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-04 23:55:13 +0300

    Слой утилит

---
+++ ZennoFramework.ApiClient/Utils/Hardware.cs
@@ -0,0 +1,10 @@
+﻿using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ZennoFramework.Api.Client.Utils
+{
+    public static class Hardware
+    {
+    }
+}
---
+++ ZennoFramework.ApiClient/Utils/RequestHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ZennoFramework.Api.Client.Utils
+{
+    public static class RequestHelper
+    {
+    }
+}
