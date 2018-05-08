0007eaff37768ea5715668979fa37b2e91a23769
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-08 16:54:42 +0300

    ...

--- ZennoFramework.ApiClient/Utils/RequestHelper.cs
+++ ZennoFramework.ApiClient/Utils/RequestHelper.cs
@@ -5,5 +5,13 @@ namespace ZennoFramework.Api.Client.Utils
 {
     public static class RequestHelper
     {
+        public static async Task<string> PostAsync(string url, string body)
+        {   
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, new StringContent(body)).ConfigureAwait(false);
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+        }
     }
 }
