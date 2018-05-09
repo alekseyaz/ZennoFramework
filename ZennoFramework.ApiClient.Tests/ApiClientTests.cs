e2e872f06e0d512058a23f9d7f3429c75b5ddeac
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-09 22:14:35 +0300

    ...

--- ZennoFramework.ApiClient.Tests/ApiClientTests.cs
+++ ZennoFramework.ApiClient.Tests/ApiClientTests.cs
@@ -9,5 +9,29 @@ namespace ZennoFramework.ApiClient.Tests
     [TestFixture]
     public class ApiClientTests
     {
+        private Api.Client.ApiClient _client = new Api.Client.ApiClient();
+
+        [Test]
+        public void Test()
+        {
+            var key = "ta";
+            var data = new GenerationData{Namespace = "test"};
+            var code = _client.GenerateCode(key, data);
+            Assert.IsNull(code);
+        }
+
+        [Test]
+        public void SerializeDeserializeTest()
+        {
+            var obj = new ResponseMessage
+            {
+                ServerError = "Check"
+            };
+
+            var json = JsonSerializer.Serialize(obj);
+            var obj2 = JsonSerializer.Deserialize<ResponseMessage>(json);
+
+            Assert.AreEqual(obj.ServerError, obj2.ServerError);
+        }
     }
 }
