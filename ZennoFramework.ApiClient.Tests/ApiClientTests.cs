ea742e6caf29b2a7419862329c81b3267865aa0c
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-09 20:11:45 +0300

    Тесты клиентского api

---
+++ ZennoFramework.ApiClient.Tests/ApiClientTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ZennoFramework.Api.Common.Messages;
+using ZennoFramework.Api.Common.Utils;
+
+namespace ZennoFramework.ApiClient.Tests
+{
+    [TestFixture]
+    public class ApiClientTests
+    {
+    }
+}
