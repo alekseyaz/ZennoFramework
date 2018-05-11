ad2805de90cab606cf518a216540059c95fa6596
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-11 12:37:15 +0300

    Тесты парсера

---
+++ ZennoFramework.Xml.Tests/Config.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+using ZennoFramework.Xml.Validation;
+
+namespace ZennoFramework.Xml.Tests
+{
+    [SetUpFixture]
+    public class Config
+    {
+    }
+}
