5e5ec3258bf9d8bc983195c880bf2b26666dd380
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-26 12:04:40 +0300

    ...

---
+++ ZennoFramework.Tests/TestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//using Test;
+using ZennoFramework.Interception;
+using ZennoFramework.Logging.Abstractions;
+using ZennoFramework.Logging.Zenno;
+using ZennoFramework.Tests.Pages.Login;
+using ZennoFramework.Tests.Pages.Login.Elements;
+using ZennoFramework.Xml;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using LogLevel = ZennoFramework.Logging.LogLevel;
+
+namespace ZennoFramework.Tests
+{
+    public class TestContext : BotContext
+    {
+    }
+}   
