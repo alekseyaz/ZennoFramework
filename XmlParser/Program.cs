15bde423322382cca18524bef56bafbec7d6d699
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-05-28 22:11:35 +0300

    Реализация xml парсера

---
+++ XmlParser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using ZennoFramework.Api.Common.Messages;
+using ZennoFramework.Generator;
+using ZennoFramework.Generator.Extensions;
+using ZennoFramework.Xml;
+using ZennoFramework.Xml.Extensions;
+using ZennoFramework.Xml.Validation;
+
+namespace XmlParser
+{
+    class Program
+    {
+    }
+}
