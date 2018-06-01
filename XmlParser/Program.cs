e88e785b5ec0f3c4df7cd5e48d20058b7750bc24
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-06-01 22:55:52 +0300

    ...

--- XmlParser/Program.cs
+++ XmlParser/Program.cs
@@ -14,5 +14,34 @@ namespace XmlParser
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            var path = @"D:\Dev\ZennoFramework\XmlParser\_elements\Elements.xml";
+
+            var docs = Loader.Load(path);
+            Validator.CheckDocuments(docs);
+            LocatorRenderer.Run(docs);
+            var docsString = docs.Values.Select(x => x.Save());
+            var data = new GenerationData{Namespace = "", XmlDocuments = docsString};
+            var code = CodeGenerator.Generate(data);
+            //Console.WriteLine(code);
+
+            foreach (var document in docs.Values)
+            {
+                WriteElements(document.Root, "  ");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void WriteElements(XElement element, string indent = "")
+        {
+            Console.WriteLine($"{indent}{element.Name.LocalName} [{element.Attribute("Key")}][{element.Attribute("x")}]");
+
+            foreach (var child in element.Elements())
+            {
+                WriteElements(child, indent + "    ");
+            }
+        }
     }
 }
