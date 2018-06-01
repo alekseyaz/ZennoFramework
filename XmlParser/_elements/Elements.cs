c7b69777d2e3e113814177e8327d8f98cee0d336
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2018-06-01 22:35:11 +0300

    Генерируемый класс

---
+++ XmlParser/_elements/Elements.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+using ZennoFramework;
+using ZennoFramework.Infrastructure.Elements;
+using ZennoFramework.Xml.Extensions;
+
+namespace MyBotNamespace.Generated
+{
+    public partial class Elements : ElementContainer<BotContext>
+    {
+    }
+}
